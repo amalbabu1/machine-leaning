94dd6a4300f30dafbf51a877fa643a53279564e9
amal babu <amalbabuporuthur@gmail.com>
2018-10-06 09:53:20 -0700

    Update ml.py

---
+++ ml.py
@@ -0,0 +1,77 @@
+import sys
+import numpy
+import pandas
+import matplotlib
+import seaborn
+import scipy
+print('Python: {}'.format(sys.version))
+print
+('Numpy: {}'.format(numpy.__version__))
+print('Pandas: {}'.format(pandas.__version__))
+print('Matplotlib: {}'.format(matplotlib.__version__))
+print('Seaborn: {}'.format(seaborn.__version__))
+print('Scipy: {}'.format(scipy.__version__))
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+data = pd.read_csv(r'E:\development\New folder\creditcard.csv')
+print(data.columns)
+data = data.sample(frac=0.1, random_state = 1)
+Fraud = data[data['Class'] == 1]
+Valid = data[data['Class'] == 0]
+outlier_fraction = len(Fraud)/float(len(Valid))
+
+
+# Get all the columns from the dataFrame
+columns = data.columns.tolist()
+
+# Filter the columns to remove data we do not want
+columns = [c for c in columns if c not in ["Class"]]
+
+# Store the variable we'll be predicting on
+target = "Class"
+
+X = data[columns]
+Y = data[target]
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.ensemble import IsolationForest
+from sklearn.neighbors import LocalOutlierFactor
+# define random states
+state = 1
+
+# define outlier detection tools to be compared
+classifiers = {
+    "Isolation Forest": IsolationForest(max_samples=len(X),
+                                        contamination=outlier_fraction,
+                                        random_state=state),
+    "Local Outlier Factor": LocalOutlierFactor(
+        n_neighbors=20,
+        contamination=outlier_fraction)}
+
+# Fit the model
+plt.figure(figsize=(9, 7))
+n_outliers = len(Fraud)
+
+
+for i, (clf_name, clf) in enumerate(classifiers.items()):
+
+    # fit the data and tag outliers
+    if clf_name == "Local Outlier Factor":
+        y_pred = clf.fit_predict(X)
+        scores_pred = clf.negative_outlier_factor_
+    else:
+        clf.fit(X)
+        scores_pred = clf.decision_function(X)
+        y_pred = clf.predict(X)
+
+    # Reshape the prediction values to 0 for valid, 1 for fraud.
+    y_pred[y_pred == 1] = 0
+    y_pred[y_pred == -1] = 1
+
+    n_errors = (y_pred != Y).sum()
+
+    # Run classification metrics
+    print('{}: {}'.format(clf_name, n_errors))
+    print(accuracy_score(Y, y_pred))
+    print(classification_report(Y, y_pred))
